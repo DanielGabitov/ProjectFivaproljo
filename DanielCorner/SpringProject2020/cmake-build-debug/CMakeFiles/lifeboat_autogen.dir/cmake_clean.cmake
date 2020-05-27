9b3ccf36429d8f995d512c508d07ba600e558651
Lebedev Egor <egorikl@list.ru>
2020-05-27 21:40:01 +0300

    Delete Daniel\'s dir

--- DanielCorner/SpringProject2020/cmake-build-debug/CMakeFiles/lifeboat_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lifeboat_autogen"
-  "lifeboat_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lifeboat_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
